823e5b8290ddec7ccee841dd3e9e64ff785f8577
Silhys <matejsilhan12@seznam.cz>
2023-10-02 18:15:11 +0200

    changed background, added multiple new buttons

--- res/js/main.js
+++ res/js/main.js
@@ -1,14 +1,18 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
-const clickupgrade = document.getElementById("clickupgrade")
+const clickupgrade = document.getElementById("clickupgrade");
+const clickupgrade2 = document.getElementById("clickupgrade2");
+const clickupgrade3 = document.getElementById("clickupgrade3");
+
 let numberOfCookies = 0;
 let clickIncrease = 1;
+let clickIncrease2 = 2;
 // {} - scope
 cookie.onclick = () => {
     //numberOfCookies = numberOfCookies + 1;
     //numberOfCookies += 1;
     // ++ inkrement
-    numberOfCookies += clickIncrease;
+    numberOfCookies = numberOfCookies + clickIncrease;
 
     counter.innerText = "Kills: " + numberOfCookies;
 }
@@ -23,3 +27,33 @@ clickupgrade.onclick = () => {
         clickIncrease++;
     }
 };
+
+clickupgrade2.onclick = () => {
+
+
+    if (numberOfCookies >= 200) {
+        //odectu susenky
+        numberOfCookies -= 200;
+        counter.innerText = "Kills: " + numberOfCookies;
+
+        numberOfCookies = numberOfCookies;
+        clickIncrease = clickIncrease + 2;
+        clickIncrease++;
+    }
+
+};
+clickupgrade3.onclick = () => {
+
+
+    if (numberOfCookies >= 500) {
+        //odectu susenky
+        numberOfCookies -= 500;
+        counter.innerText = "Kills: " + numberOfCookies;
+
+        numberOfCookies = numberOfCookies;
+        clickIncrease = clickIncrease + 3;
+        clickIncrease++;
+    }
+
+};
+
